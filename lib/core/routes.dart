9733c9b32be7986e4d9e66960f8baf5f1e95c0c9
Gabriel <gabrielvasco906@gmail.com>
2025-08-28 21:31:11 -0300

    feat: Implement routing and views for video editor application; add dependencies for navigation and audio processing

---
+++ lib/core/routes.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+import '../view/views.dart';
+
+final GoRouter router = GoRouter(
+  routes: <RouteBase>[
+    GoRoute(
+      path: '/',
+      builder: (BuildContext context, GoRouterState state) {
+        return const SplashView();
+      },
+      routes: <RouteBase>[
+        GoRoute(
+          path: 'home',
+          builder: (BuildContext context, GoRouterState state) {
+            return const HomeView();
+          },
+        ),
+        GoRoute(
+          path: 'login',
+          builder: (BuildContext context, GoRouterState state) {
+            return const LoginView();
+          },
+        ),
+        GoRoute(
+          path: 'editor',
+          builder: (BuildContext context, GoRouterState state) {
+            return const EditorView();
+          },
+        ),
+      ],
+    ),
+  ],
+);
---
+++ lib/view/views.dart
@@ -0,0 +1,4 @@
+export 'editor/editor_view.dart';
+export 'home/home_view.dart';
+export 'login/login_view.dart';
+export 'splash/splash_view.dart';
